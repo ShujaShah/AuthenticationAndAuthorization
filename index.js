ce9506c28b4cb6678822dec5799aeebdf81b2030
shujashah <shuja.ashraf11@gmail.com>
2024-01-17 23:29:43 +0530

    bug fixes

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require('express');
 require('dotenv').config();
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const dbConn = require('./src/bin/connection').dbConn;
 
+// Express configurations
 var app = express();
-app.use(express.json());
+
+//body paser
+app.use(express.json({ limit: '50mb' }));
+app.use(cookieParser());
+
+//implement cors
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    optionSuccessStatus: 200,
+  })
+);
+
+app.get('/test', (req, res, next) => {
+  res.status(200).json({
+    message: 'hello world',
+    success: 'true',
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
