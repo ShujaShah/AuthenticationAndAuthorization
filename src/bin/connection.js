b621018bd0749b21bf2074ea285e21288fd68606
shujashah <shuja.ashraf11@gmail.com>
2024-01-18 19:21:51 +0530

    added colors

--- src/bin/connection.js
+++ src/bin/connection.js
@@ -4,9 +4,13 @@ let mongo_url = process.env.MONGO_URL;
 
 mongoose
   .connect(mongo_url)
-  .then(() => console.log(colors.bgGreen('Connected to the Database...😀')))
+  .then(() =>
+    console.log(colors.bgGreen.bold.italic('Connected to the Database...😀'))
+  )
   .catch((err) =>
-    console.log(colors.bgRed('Error connecting the database💥', err))
+    console.log(
+      colors.bgRed.bold.italic('Error connecting the database💥', err)
+    )
   );
 
 module.exports = { dbConn: mongoose.connection };
